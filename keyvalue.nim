e2a0a17c733e8a8bafbc354fdcc7b62f461341d9
Carlo Capocasa <carlo@capocasa.net>
2024-10-10 22:19:01 +0200

    use new limdb type feature

--- keyvalue.nim
+++ keyvalue.nim
@@ -4,35 +4,21 @@ import limdb, at
 
 export limdb, at
 
-# LimDB requires some boilerplate because it only supports strings
-iterator keys*(a: limdb.Database): Time =
-  for k in limdb.keys(a):
-    yield k.parseFloat.fromUnixFloat
-proc del*(a: limdb.Database, t: Time) =
-  a.del $t.toUnixFloat
+type
+  LimAt = At[Database[Time, string], Database[string, Time]]
 
-# TODO: consider using at's binary serialization
-# with a custom comparison function that sorts these properly
-proc `[]`*(a: limdb.Database, t: Time): string =
-  limdb.`[]`(a, $t.toUnixFloat)
-proc `[]`*(a: limdb.Database, s: string): Time =
-  limdb.`[]`(a, s).parseFloat.fromUnixFloat
-proc `[]=`*(a: limdb.Database, t: Time, s: string) =
-  limdb.`[]=`(a, $t.toUnixFloat, s)
-proc `[]=`*(a: limdb.Database, s: string, t: Time) =
-  limdb.`[]=`(a, s, $t.toUnixFloat)
+proc initKeyValue*(kvPath: string):(Database[string,string], LimAt) =
 
-proc initKeyValue*(kvPath: string):(Database, At[Database, Database]) =
-  let kv = initDatabase(kvPath, "kv")
-  let expiry:At[Database, Database] = block:
+  let l = initDatabase(kvPath, (kv: string, t2s: Time, string, s2t: string, Time))
+
+  let expiry:LimAt = block:
     proc trigger(t: Time, k: string) =
       try:
-        kv.del k
+        del l.kv, k
       except KeyError:
         discard
-    initAt(kv.initDatabase("expiry"), kv.initDatabase("expiry_"))
+    initAt(l.t2s, l.s2t)
   asyncCheck expiry.process()
-  (kv, expiry)
-
+  (l.kv, expiry)
 
 
